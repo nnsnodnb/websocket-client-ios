7d0ce6f471faaac8c2060598ad32ae13e6574a96
nnsnodnb <nnsnodnb@gmail.com>
2023-04-14 23:10:04 +0900

    Add AppDelegate for UIApplicationDelegateAdaptor

---
+++ WebSocketClient/Sources/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  WebSocketClient
+//
+//  Created by Yuya Oka on 2023/04/14.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        return true
+    }
+}
--- WebSocketClient/Sources/WebSocketClientApp.swift
+++ WebSocketClient/Sources/WebSocketClientApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct WebSocketClientApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
