530964fd5d6799b0a47a7156536bae564c881d79
nnsnodnb <nnsnodnb@gmail.com>
2023-04-14 22:32:51 +0900

    Initial commit

---
+++ WebSocketClient/WebSocketClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketClientApp.swift
+//  WebSocketClient
+//
+//  Created by Yuya Oka on 2023/04/14.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
