6b174cd4269193c03fd8bbabfd44015bc88d2120
nnsnodnb <nnsnodnb@gmail.com>
2023-04-16 19:51:53 +0900

    Add WebSocketDataStore

---
+++ WebSocketClient/DataStores/WebSocket/WebSocketDataStore.swift
@@ -0,0 +1,14 @@
+//
+//  WebSocketDataStore.swift
+//  WebSocketClient
+//
+//  Created by Yuya Oka on 2023/04/15.
+//
+
+import Foundation
+
+protocol WebSocketDataStoreProtocol {
+}
+
+actor WebSocketDataStore: WebSocketDataStoreProtocol {
+}
