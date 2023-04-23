099ce8d410a4fafc1fd3e5666e215eef6806d142
nnsnodnb <nnsnodnb@gmail.com>
2023-04-23 18:20:19 +0900

    Add ApplicationClient

---
+++ WebSocketClient/Sources/Clients/Application/ApplicationClient.swift
@@ -0,0 +1,32 @@
+//
+//  ApplicationClient.swift
+//  WebSocketClient
+//
+//  Created by Yuya Oka on 2023/04/23.
+//
+
+import ComposableArchitecture
+import UIKit
+import XCTestDynamicOverlay
+
+struct ApplicationClient {
+    var canOpenURL: (URL) -> Bool
+    var open: @Sendable (URL) async -> Bool
+}
+
+// MARK: - DependencyKey
+extension ApplicationClient: DependencyKey {
+    public static var liveValue: Self {
+        return Self(
+            canOpenURL: { UIApplication.shared.canOpenURL($0) },
+            open: { @MainActor in await UIApplication.shared.open($0) }
+        )
+    }
+
+    public static var testValue: Self {
+        return Self(
+            canOpenURL: unimplemented("\(Self.self).canOpenURL"),
+            open: unimplemented("\(Self.self).open")
+        )
+    }
+}
--- WebSocketClient/Sources/Extensions/DependencyValues+Extension.swift
+++ WebSocketClient/Sources/Extensions/DependencyValues+Extension.swift
@@ -9,6 +9,13 @@ import ComposableArchitecture
 import Foundation
 
 extension DependencyValues {
+    var application: ApplicationClient {
+        get { self[ApplicationClient.self] }
+        set { self[ApplicationClient.self] = newValue }
+    }
+}
+
+extension DependencyValues {
     var databaseClient: DatabaseClient {
         get { self[DatabaseClient.self] }
         set { self[DatabaseClient.self] = newValue }
