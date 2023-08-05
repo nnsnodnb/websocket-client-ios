76fb7d1a8377a4b9741d92bc63025ac8067a0ae8
nnsnodnb <nnsnodnb@gmail.com>
2023-08-06 03:57:09 +0900

    Replace origin SwiftGenPlugin

--- WebSocketClientPackage/Package.swift
+++ WebSocketClientPackage/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/mono0926/LicensePlist.git", exact: "3.24.9"),
         .package(url: "https://github.com/vsanthanam/SafariView.git", exact: "1.0.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", exact: "4.1.1"),
-        .package(url: "https://github.com/BookBeat/SwiftGenPlugin.git", branch: "xcodeproject-support"),
+        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin.git", branch: "6.6.2"),
         .package(url: "https://github.com/realm/SwiftLint.git", exact: "0.51.0"),
         .package(url: "https://github.com/pointfreeco/swiftui-navigation.git", exact: "0.7.1"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "0.52.0"),
