3158f24a408bdd66b9bf2868916a9460e9a4b7f3
nnsnodnb <nnsnodnb@gmail.com>
2023-11-15 11:46:26 +0900

    Disable `trailing_comma` in Package.swift

--- WebSocketClientPackage/Package.swift
+++ WebSocketClientPackage/Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// swiftlint:disable trailing_comma
 
 import PackageDescription
 
@@ -65,3 +66,4 @@ let package = Package(
         ),
     ]
 )
+// swiftlint:enable trailing_comma
